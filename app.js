c7fe9de6a03c66339a7c8de25dd6bee2e77d4809
Jarrod Overson <joverson@onerecovery.com>
2012-06-22 00:30:31 -0700

    cleanup/redis

--- app.js
+++ app.js
@@ -4,62 +4,78 @@
 
 var express = require('express')
   , SessionStore = require('connect-redis')(express)
-//  , passport = require('passport')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , Nohm = require('nohm').Nohm
+  , redis = require('redis')
+  ;
 
-var app = module.exports = express.createServer();
+var app = module.exports = express.createServer()
+  , redisClient = redis.createClient()
+  ;
+
+// Setup Nohm with our redis connection
+
+redisClient.on("connect", function () {
+  console.log("Connected to redis")
+  Nohm.setClient(redisClient);
+}).on("error", function (err) {
+  console.log("RedisClient Error " + err);
+});
 
 // Configuration
+
 app.configure(function () {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.set('view options', { layout : false });
+  app.use(express.bodyParser());
   app.use(express.cookieParser('secret'));
   app.use(express.static(__dirname + '/public'));
   app.use(express.session({ secret : 'secret', store : new SessionStore() }));
-  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.compiler({ src : __dirname + '/public', enable : ['less'] }));
-  app.use(app.router);
-
-  //The Start Of the Local Passport Authentication
-  // - Would like to implement the oauth Strategy Here
-  // http://passportjs.org/guide/oauth.html
-  // app.use(passport.initialize());
-  // app.use(passport.session());
 });
 
 app.configure('development', function () {
-  app.use(express.logger({format : 'dev'}));
+  app.use(express.logger('dev'));
   app.use(express.errorHandler({
-    dumpExceptions : true, showStack : true
+    dumpExceptions : true,
+    showStack : true
   }));
 });
 
 app.configure('production', function () {
+  app.use(express.logger());
   app.use(express.errorHandler());
 });
 
-app.error(function (err, req, res, next) {
-  if (err instanceof NotFound) {
-    res.render('error/404.jade', { title : 'Not found 404' });
-  } else {
-    res.render('error/500.jade', { title : 'Error', error : err });
-  }
-});
+
+// Authentication handler
+
+function auth(req,res,next) {
+  if (req.session.authenticated) return next();
+  res.redirect('/login');
+}
+
+function noauth(req,res,next) {
+  if (!req.session.authenticated) return next();
+  res.redirect('/');
+}
+
 
 // Routes
 
-app.get('/', routes.index);
-app.get('/home', routes.home);
-app.get('/logout', routes.logout);
-app.get('/users', routes.users.get);
+app.get(  '/',          routes.index);
+app.get(  '/logout',    routes.logout);
+app.get(  '/login',     routes.login.get);
+app.post( '/login',     routes.login.post);
+app.get(  '/register',  routes.register.get);
+app.post( '/register',  routes.register.post);
 
-app.get('/login', routes.login.get);
-app.post('/login', routes.login.post);
-app.get('/register', routes.register.get);
-app.post('/register', routes.register.post);
+app.get('/home',  auth, routes.home);
+app.get('/users', auth, routes.users.get);
 
+app.all('/*', routes.notFound);
 
 app.listen(3000, '127.0.0.1', function () {
   console.log(
